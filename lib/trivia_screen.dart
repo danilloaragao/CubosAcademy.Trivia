7faecdc311b27abde79053b8597ff2bbd3b428c5
Danillo Aragão <danillo.aragao@rarolabs.com.br>
2021-02-03 22:58:22 -0300

    App - Finalizado

--- lib/trivia_screen.dart
+++ lib/trivia_screen.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:trivia_academy/question.dart';
+import 'package:trivia_academy/score_screen.dart';
 
 class TriviaScreen extends StatefulWidget {
   @override
@@ -7,6 +9,15 @@ class TriviaScreen extends StatefulWidget {
 
 class _TriviaScreenState extends State<TriviaScreen> {
   int answer = 0;
+  int score = 0;
+  int index = 0;
+  List<Question> questionsList = Question.getQuestionList();
+
+  void verifyResponse() {
+    if (answer == questionsList[index].answer) {
+      score++;
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -22,19 +33,20 @@ class _TriviaScreenState extends State<TriviaScreen> {
       body: Column(
         children: [
           Container(
+            width: double.infinity,
             margin: EdgeInsets.fromLTRB(32, 56, 32, 0),
             child: Column(
               crossAxisAlignment: CrossAxisAlignment.start,
               children: [
                 Text(
-                  'PERGUNTA 1:',
+                  'PERGUNTA ' + (index + 1).toString() + ' :',
                   style: TextStyle(fontSize: 16, color: Color(0xff4C4C4C)),
                 ),
                 SizedBox(
                   height: 16,
                 ),
                 Text(
-                  'O que é Flutter?',
+                  questionsList[index].questionText,
                   style: TextStyle(fontSize: 18),
                 ),
               ],
@@ -60,7 +72,7 @@ class _TriviaScreenState extends State<TriviaScreen> {
                   child: RadioListTile(
                     value: 1,
                     groupValue: answer,
-                    title: Text('É um framework da Google'),
+                    title: Text(questionsList[index].option1),
                     onChanged: (int value) {
                       setState(() {
                         answer = value;
@@ -85,7 +97,57 @@ class _TriviaScreenState extends State<TriviaScreen> {
                   child: RadioListTile(
                     value: 2,
                     groupValue: answer,
-                    title: Text('É um novo sistema operacional'),
+                    title: Text(questionsList[index].option2),
+                    onChanged: (int value) {
+                      setState(() {
+                        answer = value;
+                      });
+                    },
+                  ),
+                ),
+                Container(
+                  margin: EdgeInsets.fromLTRB(32, 10, 32, 10),
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(20),
+                    boxShadow: [
+                      BoxShadow(
+                        color: Colors.grey.withOpacity(0.15),
+                        spreadRadius: 2,
+                        blurRadius: 3,
+                        offset: Offset(1, 3),
+                      ),
+                    ],
+                    color: Color(0xffFFFFFF),
+                  ),
+                  child: RadioListTile(
+                    value: 3,
+                    groupValue: answer,
+                    title: Text(questionsList[index].option3),
+                    onChanged: (int value) {
+                      setState(() {
+                        answer = value;
+                      });
+                    },
+                  ),
+                ),
+                Container(
+                  margin: EdgeInsets.fromLTRB(32, 10, 32, 10),
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(20),
+                    boxShadow: [
+                      BoxShadow(
+                        color: Colors.grey.withOpacity(0.15),
+                        spreadRadius: 2,
+                        blurRadius: 3,
+                        offset: Offset(1, 3),
+                      ),
+                    ],
+                    color: Color(0xffFFFFFF),
+                  ),
+                  child: RadioListTile(
+                    value: 4,
+                    groupValue: answer,
+                    title: Text(questionsList[index].option4),
                     onChanged: (int value) {
                       setState(() {
                         answer = value;
@@ -94,6 +156,48 @@ class _TriviaScreenState extends State<TriviaScreen> {
                   ),
                 ),
               ],
+            ),
+          ),
+          Container(
+            height: 87,
+            decoration: BoxDecoration(color: Colors.white, boxShadow: [
+              BoxShadow(
+                color: Colors.grey.withOpacity(0.25),
+                spreadRadius: 3,
+                blurRadius: 5,
+                offset: Offset(0, -1),
+              ),
+            ]),
+            child: Center(
+              child: FlatButton(
+                padding: EdgeInsets.fromLTRB(60, 11, 60, 11),
+                shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(20)),
+                textColor: Color(0xffF7F7F7),
+                color: Color(0xffDA0175),
+                onPressed: () {
+                  verifyResponse();
+                  setState(() {
+                    if (index + 1 < questionsList.length) {
+                      index++;
+                      answer = 0;
+                    } else {
+                      Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                            builder: (context) => ScoreScreen(
+                                  result: score,
+                                  totalQuestions: questionsList.length,
+                                )),
+                      );
+                    }
+                  });
+                },
+                child: Text(
+                  'Responder',
+                  style: TextStyle(fontSize: 14),
+                ),
+              ),
             ),
           ),
         ],
